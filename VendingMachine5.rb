d81606703859d3957c9061593629bfb95eb5c991
Yusuke Yamane <yusukeyamane01@gmail.com>
2020-09-28 17:17:43 +0900

    Update VendingMachine5.rb

--- VendingMachine5.rb
+++ VendingMachine5.rb
@@ -1,79 +1,4 @@
-# # 5章問題用コード
-# class Beverage
-#   @@tax = 0.1
-#   attr_reader :name, :price
-
-#   def initialize(args)
-#     @name = args.fetch(:name, 'サイダー')
-#     @price = args.fetch(:price, 120)
-#   end
-
-#   def price
-#     @price + (@price * @@tax).floor
-#   end
-# end
-
-# class VendingMachine
-#   attr_reader :products
-
-#   def initialize(products)
-#     @products = products
-#   end
-
-#   def serve_item
-#     display_anounce
-#     product = take_order
-#     p "#{product.name}が選択されました"
-
-#     # 適正金額が受け取れるまで繰り返す。
-#     while true do
-#       payment = take_money
-#       charge = calc_change(payment, product.price)
-#       if charge >= 0
-#         break
-#       else
-#         puts "入力金額が不足しています。金額を再入力してください。"
-#       end
-#     end
-
-#     puts "お釣りは#{charge}円です。ご利用ありがとうございました。"
-#   end
-
-#   private
-
-#   attr_reader :products
-
-#   def display_anounce
-#     puts '購入したい商品を以下から選んで、金額を入力してください'
-#     products.each_with_index do |product, i|
-#       puts "[#{i}] 商品名：#{product.name} 税込み価格：#{product.price}円"
-#     end
-#   end
-
-#   def take_order
-#     products[gets.to_i]
-#   end
-
-#   def take_money
-#     gets.to_i
-#   end
-
-#   def calc_change(payment, price)
-#     payment - price
-#   end
-# end
-
-# beverages = [
-#   { name: 'コーラ', price: 120 },
-#   { name: 'お茶', price: 100 },
-#   { name: 'お水', price: 80 }
-# ]
-
-# @beverages = beverages.map{ |b| Beverage.new(name: b[:name], price: b[:price]) }
-# vm = VendingMachine.new(@beverages)
-# vm.serve_item
-
-# 5章解答用コード
+# 5章問題用コード
 class Beverage
   @@tax = 0.1
   attr_reader :name, :price
@@ -90,15 +15,15 @@ end
 
 class Tabaco
   @@tax = 0.6
-  attr_reader :name, :price, :tax
+  attr_reader :brand, :cost
 
   def initialize(args)
-    @name = args.fetch(:name, 'テックタバコ')
-    @price = args.fetch(:price, 300)
+    @brand = args.fetch(:brand, 'テックタバコ')
+    @cost = args.fetch(:cost, 300)
   end
 
-  def price
-    @price + (@price * @@tax).floor
+  def cost
+    @cost + (@cost * @@tax).floor
   end
 end
 
@@ -109,10 +34,23 @@ class VendingMachine
     @products = products
   end
 
-  def transaction
+  def serve_item
     display_anounce
     product = take_order
-    serve_item(product)
+    p "#{product.name}が選択されました"
+
+    # 適正金額が受け取れるまで繰り返す。
+    while true do
+      payment = take_money
+      charge = calc_change(payment, product.price)
+      if charge >= 0
+        break
+      else
+        puts "入力金額が不足しています。金額を再入力してください。"
+      end
+    end
+
+    puts "お釣りは#{charge}円です。ご利用ありがとうございました。"
   end
 
   private
@@ -130,19 +68,8 @@ class VendingMachine
     products[gets.to_i]
   end
 
-  def serve_item(product)
-    # 適正金額が受け取れるまで繰り返す。
-    while true do
-      payment = gets.to_i
-      charge = calc_change(payment, product.price)
-      if charge >= 0
-        break
-      else
-        puts "入力金額が不足しています。金額を再入力してください。"
-      end
-    end
-
-    puts "お釣りは#{charge}円です。ご利用ありがとうございました。"
+  def take_money
+    gets.to_i
   end
 
   def calc_change(payment, price)
@@ -151,11 +78,98 @@ class VendingMachine
 end
 
 tabacos = [
-  { name: '第一タバコ', price: 120 },
-  { name: 'サムライ', price: 100 },
-  { name: 'キャッツアイ', price: 80 }
+  { brand: '第一タバコ', cost: 120 },
+  { brand: 'サムライ', cost: 100 },
+  { brand: 'キャッツアイ', cost: 80 }
 ]
 
-@tabacos = tabacos.map{ |b| Tabaco.new(name: b[:name], price: b[:price]) }
-vm = VendingMachine.new(@tabacos)
-vm.transaction
+@tabacos = tabacos.map{ |b| Tabaco.new(name: b[:brand], price: b[:cost]) }
+vm = VendingMachine.new(@beverages)
+vm.serve_item
+
+# 5章解答用コード
+# class Beverage
+#   @@tax = 0.1
+#   attr_reader :name, :price
+
+#   def initialize(args)
+#     @name = args.fetch(:name, 'サイダー')
+#     @price = args.fetch(:price, 120)
+#   end
+
+#   def price
+#     @price + (@price * @@tax).floor
+#   end
+# end
+
+# class Tabaco
+#   @@tax = 0.6
+#   attr_reader :name, :price, :tax
+
+#   def initialize(args)
+#     @name = args.fetch(:name, 'テックタバコ')
+#     @price = args.fetch(:price, 300)
+#   end
+
+#   def price
+#     @price + (@price * @@tax).floor
+#   end
+# end
+
+# class VendingMachine
+#   attr_reader :products
+
+#   def initialize(products)
+#     @products = products
+#   end
+
+#   def transaction
+#     display_anounce
+#     product = take_order
+#     serve_item(product)
+#   end
+
+#   private
+
+#   attr_reader :products
+
+#   def display_anounce
+#     puts '購入したい商品を以下から選んで、金額を入力してください'
+#     products.each_with_index do |product, i|
+#       puts "[#{i}] 商品名：#{product.name} 税込み価格：#{product.price}円"
+#     end
+#   end
+
+#   def take_order
+#     products[gets.to_i]
+#   end
+
+#   def serve_item(product)
+#     # 適正金額が受け取れるまで繰り返す。
+#     while true do
+#       payment = gets.to_i
+#       charge = calc_change(payment, product.price)
+#       if charge >= 0
+#         break
+#       else
+#         puts "入力金額が不足しています。金額を再入力してください。"
+#       end
+#     end
+
+#     puts "お釣りは#{charge}円です。ご利用ありがとうございました。"
+#   end
+
+#   def calc_change(payment, price)
+#     payment - price
+#   end
+# end
+
+# tabacos = [
+#   { name: '第一タバコ', price: 120 },
+#   { name: 'サムライ', price: 100 },
+#   { name: 'キャッツアイ', price: 80 }
+# ]
+
+# @tabacos = tabacos.map{ |b| Tabaco.new(name: b[:name], price: b[:price]) }
+# vm = VendingMachine.new(@tabacos)
+# vm.transaction
