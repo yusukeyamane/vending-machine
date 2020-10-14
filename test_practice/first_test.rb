52ae9edfb6536c6dd952dbbedc0081fec8e169fb
Yusuke Yamane <yusukeyamane01@gmail.com>
2020-10-14 11:22:52 +0900

    Update first_test.rb

--- test_practice/first_test.rb
+++ test_practice/first_test.rb
@@ -2,27 +2,31 @@ require './VendingMachine'
 require 'minitest/autorun'
 
 class ProductTest < Minitest::Test
+  def setup
+    @coke = Beverage.new(name: 'コーラ', price: 120)
+  end
+
   def test_name
-    coke = Beverage.new(name: 'コーラ', price: 120)
-    assert_equal 'コーラ', coke.name
+    assert_equal 'コーラ', @coke.name
   end
 
   def test_price
-    coke = Beverage.new(name: 'コーラ', price: 120)
-    assert_equal 132, coke.price
+    assert_equal 132, @coke.price
   end
 end
 
 class VendingMachineTest < Minitest::Test
-  def test_transaction
+  def setup
     beverages = [
       { name: 'コーラ', price: 120 },
       { name: 'お茶', price: 100 },
       { name: 'お水', price: 80 }
     ]
-    products = beverages.map { |b| Beverage.new({ name: b[:name], price: b[:price] }) }
+    @products = beverages.map { |b| Beverage.new({ name: b[:name], price: b[:price] }) }
+  end
 
-    vm = VendingMachine.new(products)
+  def test_transaction
+    vm = VendingMachine.new(@products)
     assert_instance_of(Beverage, vm.transaction)
   end
 end
